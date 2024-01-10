be62e297eae80e3435943484e22edd7deef16387
Joseline0609 <jkgarciaalfaro@gmail.com>
2024-01-10 14:35:09 -0600

    names to images added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
-import data from './data/dataset.js';
+import data from './data/datasetAI.js';
 
 console.log(example, renderItems(data), data);
