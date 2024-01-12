1ee30b206c4acd2b8cc0c04cc83d545e8cfc4eda
Etelbina <etelbinacanedo@gmail.com>
2024-01-12 15:07:46 -0600

    Header and card styles

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
+import data from './data/datasetAI.js';
 
-import data from './data/dataset.js';
 
-console.log(example, renderItems(data), data);
+document.querySelector('section[name="cards"]').appendChild(renderItems(data))
+//console.log(renderItems(data), );
+/*
+const seeMoreButton = document.getElementById("seeMoreButton");
+seeMoreButton.addEventListener("click", function seeMore() {
+    // document.getElementsByID("front-card").className = "front-card hide";
+    // document.getElementsByID("back-card").className = "back-card show";
+});*/
+
+
+//renderItems(data)
