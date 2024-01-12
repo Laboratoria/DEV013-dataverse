5de4480162cca7618ca70c85dcfe5edf3393a17c
Joseline0609 <jkgarciaalfaro@gmail.com>
2024-01-12 10:33:17 -0600

    category styles added and data corrections

--- src/main.js
+++ src/main.js
@@ -2,14 +2,9 @@ import { renderItems } from './view.js';
 import data from './data/datasetAI.js';
 
 
-document.querySelector('section[name="cards"]').appendChild(renderItems(data))
-//console.log(renderItems(data), );
-/*
-const seeMoreButton = document.getElementById("seeMoreButton");
-seeMoreButton.addEventListener("click", function seeMore() {
-    // document.getElementsByID("front-card").className = "front-card hide";
-    // document.getElementsByID("back-card").className = "back-card show";
-});*/
+document.addEventListener('DOMContentLoaded', () => {
+  const tarjetas = renderItems(data);
 
-
-//renderItems(data)
+  // para que se agregue las tarjetas como hijas del elemento main
+  document.body.appendChild(tarjetas);  
+});
