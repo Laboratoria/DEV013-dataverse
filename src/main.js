ee67a7dfc4b5de956cc410a0361c70245aa45331
Joseline0609 <jkgarciaalfaro@gmail.com>
2024-01-15 16:47:17 -0600

    test

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import { renderItems } from './view.js';
 import data from './data/datasetAI.js';
 
-//seleccionar el lugar donde se va a imprimir la data 
-//puede ser con query selector
+//seleccionar el lugar donde se va a imprimir la data (puede ser con query selector)
 
 const cards=renderItems(data);
 
 document.body.appendChild(cards);
 
+seleccionar el boton en
+//agregar un event listener al los botones 
+// el evento a escuchar seria un click 
+//al hacer click se ejecuta la funcion que debe hacer girar la tarjeta 
+
+function turnCard() {
+
+};
+
+
+button-container.addEventListener('click',turnCard)
 
 
 
