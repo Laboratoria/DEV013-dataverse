ba1e4001eca87ba486eedc6bee60355b611ae804
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2024-01-16 11:03:34 -0500

    se muestran las cards en las web con datos

--- src/view.js
+++ src/view.js
@@ -1,14 +1,24 @@
 export const renderItems = (data) => {
   // Aquí comienza tu código y puedes retornar lo que tu necesites
+  let html = ""; //contenedor
+  data.forEach((cartas) => { // iterar datos, recorrer los datos  dl = define lista  dd = descripcion
+    html += `                 
+    <dl itemscope itemtype="Cards">
+    <dd itemprop="imagenUrl">
+    <img class="img-cartas" src=${cartas.imageUrl} alt=${cartas.name}>
+    </dd>
+    <dt>Nombre:</dt>
+    <dd itemprop="name">${cartas.name} </dd>
+    <dt>Descripción:</dt>
+    <dd itemprop="description">${cartas.shortDescription}</dd>
+    <dt>Poder:</dt>
+    <dd itemprop="power">${cartas.facts.powerFul}</dd>
+    </dl>`
+    //
+  });
+  return html
 
-  //-----------------------------------------------------------
 
-  console.log(data);
-  const listNames = data.map((item) => item.name);
-  const listDesc = data.map((item) => item.shortDescription);
-
-  return {
-    names: listNames,
-    description: listDesc,
-  };
 };
+
+
