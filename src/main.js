ba621e6e2ddef897aae740c001e7a78b2a997e05
Elizabeth <elizabethepj@gmail.com>
2024-01-16 11:01:59 -0500

    modificacionesViewRende

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import { example } from './dataFunctions.js';
+/*import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
-
-console.log(example, renderItems(data), data);
+console.log(example, renderItems(data), data);*/
