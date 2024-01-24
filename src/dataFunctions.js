933fce6a055397d38c743b8e638cde30327a5d4d
Joseline0609 <jkgarciaalfaro@gmail.com>
2024-01-24 16:53:41 -0600

    Rotate cards feature added but this time using event delegation

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -2,18 +2,18 @@
 `filterData(data, filterBy, value)`: esta función recibe tres parámetros. 
 El primer parámetro,`data`, nos entrega los datos. 
 El segundo parámetro,`filterBy`, nos dice con respecto a cuál de los campos de la data se quiere filtrar.
-El tercer parámetro,`value`, indica el valor de campo que queremos filtrar.*/
+El tercer parámetro,`value`, indica el valor de campo que queremos filtrar.
 
-//recorrer el array (ya lo hace el filter) 
-//por cada objeto hacer la comparacion de si la propiedad por que esta siendo filtrada tiene el valor buscado 
-//ejemplo, si se esta filtrando por categoria, esta corresponde al "campo" es decir "filterBy"
-//el value seria el valor de esa propiedad especifica (ejemplo "ornamental")
-//si coincide retornara true y se guardara en el nuevo array 
-export const filterData = (data, filterBy, value) => {
+recorrer el array
+por cada objeto hacer la comparacion de si la propiedad por que esta siendo filtrada tiene el valor buscado 
+ejemplo, si se esta filtrando por categoria, esta corresponde al "campo" es decir "filterBy"
+el value seria el valor de esa propiedad especifica (ejemplo "ornamental")
+si coincide retornara true y se guardara en el nuevo array */
+
+export const filterData = (data, filterBy, value) => 
+{
   return data.filter(objeto => objeto[filterBy] === value);
 }
-//luego hay que actuaizar la interfaz y que solo se rendericen las tarjetas del nuevo array
-//se podria agregar un onclick al boton y que al hacer click se ejecute la funcion pero con los parametros correspondientes
 
 
 /*`sortData(data, sortBy, sortOrder)`: esta función`sort`u ordenar recibe tres parámetros.
@@ -21,8 +21,9 @@ export const filterData = (data, filterBy, value) => {
  El segundo parámetro,`sortBy`, nos dice con respecto a cuál de los campos de la data se quiere ordenar.
   El tercer parámetro,`sortOrder`, indica si se quiere ordenar de manera ascendente o descendente.*/
 
-/*export const sortData = () => {
-  
+/*export const sortData = (data, sortBy, sortOrder) => {
+  return data.sort()
+
 };
 */
 
--- src/view.js
+++ src/view.js
@@ -50,7 +50,7 @@ export const renderItems = (data) => {
       </div>
       <p>${element.shortDescription}</p>
       <div class="button-container">
-        <button id="detalles" onclick="turnCard(this)" >Detalles</button>
+        <button id="detalles" class="detalles">Detalles</button>
       </div>
     </article>
     <article id="back-card" class="hide" class="back-card">
@@ -69,12 +69,25 @@ export const renderItems = (data) => {
         <dt>Datos climáticos</dt><dd>${element.climaticData}</dd>
         <dt>Mantenimiento</dt><dd>${element.maintenanceNeeds}</dd>
         </dl>
+
         <div id="icons">
           <div class="stats">
             <img class="stats1" alt="Estadisticas" src="resources/Icons/estadisticas.png">
-            <img class="stats2" alt="Descripcion" src="resources/Icons/descripcion.png">
+
+          <div>
+            <img class="openModalBtn" id="openModalBtn" alt="Descripcion" src="resources/Icons/descripcion.png">
+            <div id="myModal" class="modal"> 
+              <div class="modal-content">
+                <header>
+                  <h3>${element.name}</h3>
+                </header>
+                <p>${element.description}</p>
+             </div>
+            </div>
+          </div>
+
           <div class="back">
-          <input type="image" onclick="returnCard(this)" id="Regresar" name="Regresar" alt="Regresar"  src="resources/Icons/Regresar.png">
+          <input type="image" id="Regresar" class="regresar" alt="Regresar"  src="resources/Icons/Regresar.png">
           </div>
         </div>
       </div>
@@ -88,11 +101,11 @@ export const renderItems = (data) => {
   return cardList;
 };
 
+//funcion que debe hacer girar la tarjeta 
+//usando onclick como atributo en el html
 
-//al hacer click se ejecuta la funcion que debe hacer girar la tarjeta 
-
-function turnCard(botton) {
-  const cardContainer=  botton.closest('.card-container');
+/*function turnCard(button) {
+  const cardContainer=  button.closest('.card-container');
   const frontCard=cardContainer.querySelector("#front-card");
   const backCard=cardContainer.querySelector("#back-card");
 
@@ -102,9 +115,9 @@ function turnCard(botton) {
   frontCard.classList.toggle('hide');
 }
  
-function returnCard (botton) {
+function returnCard (button) {
   //lo mismo, pero al reves xd 
-  const cardContainer=  botton.closest('.card-container');
+  const cardContainer=  button.closest('.card-container');
   const frontCard=cardContainer.querySelector("#front-card");
   const backCard=cardContainer.querySelector("#back-card")
 
@@ -114,18 +127,21 @@ function returnCard (botton) {
 
 //hacer las variable globales 
 window.turnCard=turnCard;
-window.returnCard=returnCard;
+window.returnCard=returnCard;*/
 
 
-//Ventanas Modales/Popup
+/*             <div>
+            <img class="stats1" alt="Estadisticas" src="resources/Icons/estadisticas.png">
+            <div id="myModal" class="modal"> 
+              <div class="modal-content">
+                  <h3>Estadisticas</h3>
+                  <p></p>
+             </div>
+            </div>
+          </div>*/
 
-
-/*function popUpDescription(botton){
-  const popUpDescrip= botton.closest('.card-container');
-  const popUp= popUpDescrip.querySelector('popUpDescription');
-
-
-}
-
-window.popUpDescription=popUpDescription;*/
-
+/*const btnOpenModal = cardItem.querySelector('.openModalBtn')
+    btnOpenModal.addEventListener("click", () => {
+      const MiModal = cardItem.querySelector('.modal')
+      console.log({MiModal})
+    })*/
