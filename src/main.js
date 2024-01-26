0b417cf7a3313c6b412d730bbc200c4c6a3f1c72
Addis <lumiaddis@gmail.com>
2024-01-26 20:45:11 -0300

    declare una variable para mostrar data

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
+const cardContainer = document.querySelector("#root")
+cardContainer.appendChild(renderItems(data))
+
 console.log(example, renderItems(data), data);
 renderItems(data)
